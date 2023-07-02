fe5620cda9386ef8d8a4a8880db48c5a439df58a
MAKWERK <mikkel.keldmann@hotmail.com>
2023-07-02 14:02:54 +0200

    Added comments to div_color

--- R/div_col.R
+++ R/div_col.R
@@ -1,15 +1,27 @@
-# Based on 2 inputs - type and custom color - return standard or custom color
+#' div_col
+#'
+#' @param type String indicating type for default colors
+#' @param color Custom color input - either a color from colors() or a color in hex code
+#'
+#' @return Color
+#' @export
+#'
+#' @examples div_col(color="white")
 
 div_col <- function(type = NA, color = NA) {
+  # Checks for custom color and returns it if it is a color or hex code
   if (!is.na(color) & (color %in% colors() | is_hexcolor(color))) {
     return(color)
   }
+  # Checks for input - if not found throws error
   if (is.na(type) & !(!is.na(color) & (color %in% colors() | is_hexcolor(color)))) stop("No type or color chosen")
   col <- dplyr::case_when(
     type == "bar_chart_mean" ~ "red",
     TRUE ~ NA
   )
+  # If no usable output is generated throw error
   if (is.na(col)) stop("No color chosen")
+  # Returns color
   return(col)
 }
 
