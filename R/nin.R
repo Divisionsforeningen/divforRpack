8083a0f8c4249f0e62d262b0bece0d2c47a3bb05
MAKWERK <mikkel.keldmann@hotmail.com>
2023-07-02 20:53:11 +0200

    added negate in function

---
+++ R/nin.R
@@ -0,0 +1,14 @@
+#' %nin%
+#'
+#' @param a Object to search for
+#' @param b Object to search in
+#'
+#' @return TRUE if a is in b, else FALSE
+#' @export
+#'
+#' @examples a <- 1
+#' b <- c(1, 3, 5)
+#' a %nin% b
+"%nin%" <- function(a, b) {
+  !a %in% b
+}
