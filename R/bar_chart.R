f82cfcac8658606a8e125f0440d5d260867941b0
MAKWERK <mikkel.keldmann@hotmail.com>
2023-07-03 19:29:16 +0200

    bar_chart beta

--- R/bar_chart.R
+++ R/bar_chart.R
@@ -1,11 +1,10 @@
 #' bar_chart
 #'
-#' @param df Data frame
-#' @param x
-#' @param y
+#' @param df Data frame containing KPI and Label, x and y
+#' @param x Column containing KPI
+#' @param y Label
 #' @param id Id to highlight
-#' @param z
-#' @param kpi Column containing KPI
+#' @param z Selected KPI - from shiny input
 #' @param positive True if higher is better for KPI
 #' @param fillCol Color of non-highlighted ids
 #' @param highCol Highlighted id color
@@ -13,8 +12,8 @@
 #' @return A bar plot, chosen id is highlighted, and mean is added as a red line - ordering is done by "positive"
 #' @export
 #'
-#' @examples
-bar_chart <- function(df, x, y, id, z, kpi, positive = c(TRUE, FALSE), fillCol = NA, highCol = NA) {
+#' @examples no example
+bar_chart <- function(df, x, y, id, z, positive = c(TRUE, FALSE), fillCol = NA, highCol = NA) {
   ggplot(df, aes(x = x, y = reorder(x, mean, decreasing = positive))) +
     geom_bar(stat = "identity", width = 0.3, fill = div_col("bar_chart_fill", ifelse(is.na(fillCol), NA, fillCol))) +
     geom_bar(data = df %>%
@@ -23,10 +22,10 @@ bar_chart <- function(df, x, y, id, z, kpi, positive = c(TRUE, FALSE), fillCol =
       filter(y != id), aes(), size = 6, color = div_col("bar_chart_fill", ifelse(is.na(fillCol), NA, fillCol))) +
     geom_point(data = df %>%
       filter(y == id), size = 6, color = div_col("bar_chart_highlight", ifelse(is.na(highCol), NA, highCol))) +
-    geom_vline(aes(xintercept = mean(df$kpi), color = "League average"), size = 2, alpha = 0.8) +
+    geom_vline(aes(xintercept = mean(df$x), color = "League average"), size = 2, alpha = 0.8) +
     scale_color_manual("", values = c("League average" = div_col("mean"))) +
     labs(
-      x = paste0(kpi),
+      x = paste0(z),
       y = "",
       title = "Your rank compared to the chosen teams"
     ) +
