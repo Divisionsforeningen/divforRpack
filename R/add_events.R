47970b023313f381369eb13961bc8c9d6ac72557
MAKWERK <mikkel.keldmann@hotmail.com>
2023-08-09 14:47:05 +0200

    added functionality to add_events

--- R/add_events.R
+++ R/add_events.R
@@ -12,8 +12,8 @@
 #' @param lines Set to "TRUE" to add lines from x,y to xend,yend
 #' @param bgCol Background color - set to shiny theme
 #' @param shirt Column with shirt numbers - will be added to events
-#' @param eventArgs Event arguments, usable: color and border
-#' @param shotArgs Shot arguments, usable: color and border
+#' @param eventArgs Event arguments, usable: color and outcome (0 or 1)
+#' @param shotArgs Shot arguments, usable: color and outcome ("goal" and "other")
 #' @param heatmapArgs Heat map arguments: usable alpha, color and fill
 #' @param lineArgs Lines arguments: usable linetype and color
 #'
@@ -33,9 +33,9 @@ add_events <- function(df = NA, x = NA, y = NA, xend = NA, yend = NA,
 
   # Define standard values for events, shots and heatmaps
 
-  event_args <- list(color = div_col(type = "highlight"), border = "black")
-  shot_args <- list(color = div_col(type = "goal"), border = "black")
-  heatmap_args <- list(alpha = 0.05, color = "turbo", type = "start", fill = "red")
+  event_args <- list(color = c(div_col(type = "highlight"), div_col("goal")), border = "black", outcome = NA)
+  shot_args <- list(color = c(div_col(type = "goal"), div_col(type = "fill")), border = "black", outcome = NA)
+  heatmap_args <- list(alpha = 0.1, fill = "red")
   line_args <- list(linetype = "solid", color = "black")
 
   # Replace standards with user inputs if any
@@ -43,6 +43,16 @@ add_events <- function(df = NA, x = NA, y = NA, xend = NA, yend = NA,
   event_args <- modifyList(event_args, eventArgs[intersect(names(eventArgs), names(event_args))])
   heatmap_args <- modifyList(heatmap_args, heatmapArgs[intersect(names(heatmapArgs), names(heatmap_args))])
   line_args <- modifyList(line_args, lineArgs[intersect(names(lineArgs), names(line_args))])
+
+  if (!is.na(shot_args[["outcome"]]) && length(shot_args[["color"]]) < 2) {
+    h <- list(div_col(type = "fill"))
+    shot_args[["color"]] <- append(shot_args[["color"]], h)
+  }
+
+  if (!is.na(event_args[["outcome"]]) && length(event_args[["color"]]) < 2) {
+    h <- list(event_col(type = "fill"))
+    event_args[["color"]] <- append(event_args[["color"]], h)
+  }
 
   # Create empty geom_point to serve as base
   # p=list(geom_point())
@@ -76,9 +86,14 @@ add_events <- function(df = NA, x = NA, y = NA, xend = NA, yend = NA,
       geom_segment(aes(x = 0, xend = 100.01, y = 100, yend = 100))
     )
 
-
     # Append heatmap list to output
     p <- append(p, h)
+
+    dim <- list(
+      coord_cartesian(xlim = c(0, 100), ylim = c(0, 100))
+    )
+
+    p <- append(p, dim)
   }
 
   if (lines == TRUE) {
@@ -98,22 +113,53 @@ add_events <- function(df = NA, x = NA, y = NA, xend = NA, yend = NA,
   }
 
   if (shot == F) {
-    # Throw an error if color is not usable
-    tryCatch(div_col(color = event_args[["color"]]), error = function(e) stop("Point color not usable"))
+    if (length(shot_args[["color"]]) > 1) {
+      # Throw an error if color is not usable
+      for (i in length(event_args[["color"]])) {
+        tryCatch(div_col(color = event_args[["color"]][i]), error = function(e) stop("Point color not usable"))
+      }
+    }
+    if (length(event_args[["border"]]) > 1) {
+      # Throw an error if border is not usable
+      for (i in length(event_args[["border"]])) {
+        tryCatch(div_col(color = event_args[["border"]][i]), error = function(e) stop("Border color not usable"))
+      }
+    }
 
-    # Throw an error if border is not usable
-    tryCatch(div_col(color = event_args[["border"]]), error = function(e) stop("Border color not usable"))
+    if (is.na(event_args[["outcome"]])) {
+      # Adds events via geom_point
+      e <- list(
+        geom_point(
+          data = df, aes(x = .data[[x]], y = .data[[y]]), color = event_args[["border"]][1], shape = 19, size = 6
+        ),
+        geom_point(
+          data = df, aes(x = .data[[x]], y = .data[[y]]), color = event_args[["color"]][1], shape = 19, size = 4
+        ),
+        coord_cartesian(xlim = c(0, 100), ylim = c(0, 100))
+      )
+    } else {
+      # Adds events via geom_point
+      e <- list(
+        geom_point(
+          data = df %>% filter(.data[[event_args[["outcome"]]]] == 1),
+          aes(x = .data[[x]], y = .data[[y]]), color = event_args[["border"]][1], pch = 19, size = 6
+        ),
+        geom_point(
+          data = df %>% filter(.data[[event_args[["outcome"]]]] == 1),
+          aes(x = .data[[x]], y = .data[[y]]), color = event_args[["color"]][1], pch = 19, size = 4
+        ),
+        geom_point(
+          data = df %>% filter(.data[[event_args[["outcome"]]]] == 0),
+          aes(x = .data[[x]], y = .data[[y]]), color = event_args[["border"]][1], pch = 19, size = 6
+        ),
+        geom_point(
+          data = df %>% filter(.data[[event_args[["outcome"]]]] == 0),
+          aes(x = .data[[x]], y = .data[[y]]), color = event_args[["color"]][2], pch = 19, size = 4
+        )
+      )
+    }
 
-    # Adds events via geom_point
-    e <- list(
-      geom_point(
-        data = df, aes(x = .data[[x]], y = .data[[y]]), color = event_args[["border"]], shape = 19, size = 6
-      ),
-      geom_point(
-        data = df, aes(x = .data[[x]], y = .data[[y]]), color = event_args[["color"]], shape = 19, size = 4
-      ),
-      coord_cartesian(xlim = c(0, 100), ylim = c(0, 100))
-    )
+
 
     # Append events list to output
     p <- append(p, e)
@@ -132,24 +178,61 @@ add_events <- function(df = NA, x = NA, y = NA, xend = NA, yend = NA,
     # Returns list of ggplot layers
     return(p)
   } else if (shot == TRUE) {
-    # Throw an error if color is not usable
-    tryCatch(div_col(color = shot_args[["color"]]), error = function(e) stop("Point color not usable"))
+    if (length(shot_args[["color"]]) > 1) {
+      # Throw an error if color is not usable
+      for (i in length(shot_args[["color"]])) {
+        tryCatch(div_col(color = shot_args[["color"]][i]), error = function(e) stop("Point color not usable"))
+      }
+    }
+    if (length(shot_args[["border"]]) > 1) {
+      # Throw an error if border is not usable
+      for (i in length(shot_args[["border"]])) {
+        tryCatch(div_col(color = shot_args[["border"]][i]), error = function(e) stop("Border color not usable"))
+      }
+    }
 
-    # Throw an error if border is not usable
-    tryCatch(div_col(color = shot_args[["border"]]), error = function(e) stop("Border color not usable"))
 
-    # Create list of geoms for shot map
-    s <- list(
-      geom_point(
-        data = df, aes(x = .data[[x]], y = .data[[y]]), color = shot_args[["border"]], pch = 19, size = 6
-      ),
-      geom_point(
-        data = df, aes(x = .data[[x]], y = .data[[y]]), color = shot_args[["color"]], pch = 19, size = 4
-      ),
+    if (is.na(shot_args[["outcome"]])) {
+      # Create list of geoms for shot map
+      s <- list(
+        geom_point(
+          data = df, aes(x = .data[[x]], y = .data[[y]]), color = shot_args[["border"]][1], pch = 19, size = 6
+        ),
+        geom_point(
+          data = df, aes(x = .data[[x]], y = .data[[y]]), color = shot_args[["color"]][1], pch = 19, size = 4
+        )
+      )
+    } else {
+      # Create list of geoms for shot map
+      s <- list(
+        geom_point(
+          data = df %>% filter(tolower(.data[[shot_args[["outcome"]]]]) == "goal"),
+          aes(x = .data[[x]], y = .data[[y]]), color = shot_args[["border"]][1], pch = 19, size = 6
+        ),
+        geom_point(
+          data = df %>% filter(tolower(.data[[shot_args[["outcome"]]]]) == "goal"),
+          aes(x = .data[[x]], y = .data[[y]]), color = shot_args[["color"]][1], pch = 19, size = 4
+        ),
+        geom_point(
+          data = df %>% filter(tolower(.data[[shot_args[["outcome"]]]]) != "goal"),
+          aes(x = .data[[x]], y = .data[[y]]), color = shot_args[["border"]][1], pch = 19, size = 6
+        ),
+        geom_point(
+          data = df %>% filter(tolower(.data[[shot_args[["outcome"]]]]) != "goal"),
+          aes(x = .data[[x]], y = .data[[y]]), color = shot_args[["color"]][2], pch = 19, size = 4
+        )
+      )
+    }
+
+    # Create rest of needed shot geoms
+    sh <- list(
       coord_cartesian(xlim = c(0, 100), ylim = c(0, 100)),
       coord_flip(xlim = c(49, 101)),
       scale_y_reverse()
     )
+
+    # Append to shot map
+    s <- append(s, sh)
 
     # Append shot map list to output
     p <- append(p, s)
