175eb13a13f2f36dfcda1becd22b71f5d878c5e5
MAKWERK <mikkel.keldmann@hotmail.com>
2023-08-09 11:06:44 +0200

    created physical chart - change player_dev to standard

--- R/line_chart_phys.R
+++ R/line_chart_phys.R
@@ -1,31 +1,42 @@
 line_chart_phys <- function() {
   # TODO Build function & documentation
-
-
+  # Create ggplot with date and selected variable as x and y
   ggplot(ss_summary, aes(Date, get(input$sel_phys_par), group = 1)) +
+    # Add points for selected team
     geom_point(data = ss_summary %>%
       filter(`Team Name` == as.name(id_name)), size = 2, alpha = 0.5, color = "lightgrey") +
+    # Add line between points for selected teams
     geom_line(data = ss_summary %>%
       filter(`Team Name` == as.name(id_name)), size = 1, alpha = 0.5, aes(color = "Game by game")) +
+    # Add trend for selected teams
     geom_smooth(data = ss_summary %>%
       filter(`Team Name` == as.name(id_name)), aes(color = "Team progression"), size = 2, se = FALSE) +
+    # Add trend for league
     stat_smooth(method = "lm", formula = y ~ x, level = 0.95, size = 2, aes(color = "League progression", fill = "95% confidence interval")) +
+    # Add trend for team to compare with
     geom_smooth(data = ss_summary %>%
       filter(`Team Name` == as.name(input$sel_phys_overlay)), aes(color = "Overlayed team"), size = 2, se = FALSE) +
+    # Set colors
     scale_color_manual(NULL, values = c("Overlayed team" = "yellow", "League progression" = "blue", "Team progression" = "red", "Game by game" = "lightgrey")) +
+    # Set fill color
     scale_fill_manual(NULL, values = ("95% confidence interval" <- "lightgrey")) +
+    # Break by month
     scale_x_date(
       date_breaks = "1 month",
       date_labels = "%B"
     ) +
+    # Define guides
     guides(
       color = guide_legend(override.aes = list(fill = NA), order = 1),
       fill = guide_legend(override.aes = list(color = NA), order = 2)
     ) +
+    # Add point for selected game
     geom_point(data = ss_summary %>%
       filter(`Team Name` == id_name & Date == as.Date(d_id)), size = 8, alpha = 0.4, color = "yellow") +
+    # Add label for selected game
     geom_text_repel(data = ss_summary %>%
       filter(`Team Name` == id_name & Date == as.Date(d_id)), size = 5, nudge_y = -1, aes(label = paste0(round(get(input$sel_phys_par), digits = 1)))) +
+    # Define labels and caption
     labs(
       title = "Progression during season",
       subtitle = as.name(id_name),
@@ -33,6 +44,7 @@ line_chart_phys <- function() {
       x = "",
       caption = "Data from Second Spectrum"
     ) +
+    # Theme
     theme(
       axis.text.y = element_blank(),
       plot.title = element_text(face = "bold", size = 15),
--- R/player_dev_lines.R
+++ R/player_dev_lines.R
@@ -1,6 +1,6 @@
 #' player_dev_lines
 #'
-#' @param player_df Data frame with player data
+#' @param df Data frame with player data
 #' @param competitionId Coloum name of Wyscout competitionId
 #' @param season Coloum name of x axis
 #' @param value Coloum name of y axis
@@ -11,23 +11,25 @@
 #' @return Plot of KPI by season - facet_wrap by competition - order by age and level
 #' @export
 #'
-player_dev_lines <- function(player_df, competitionId = "competitionId", season = "season", value = "value", comp = "comp", KPI = "Put KPI here", provider = "Put provider here") {
+player_dev_lines <- function(df, competitionId = "competitionId", season = "season", value = "value", comp = "comp", KPI = "Put KPI here", provider = "Put provider here") {
+  # TODO Write test battery
+
   # Get ordering from divforRpack::comp_order()
-  player_df <- player_df %>% mutate(order = divforRpack::comp_order(.data[[competitionId]]))
+  df <- df %>% mutate(order = divforRpack::comp_order(.data[[competitionId]]))
 
   # Create values for factoring
-  labels <- as.list(unique(player_df %>% dplyr::arrange(order) %>% dplyr::select(.data[[comp]]))) %>% unlist()
-  levels <- as.list(unique(player_df %>% dplyr::arrange(order) %>% select(order))) %>% unlist()
+  labels <- as.list(unique(df %>% dplyr::arrange(order) %>% dplyr::select(.data[[comp]]))) %>% unlist()
+  levels <- as.list(unique(df %>% dplyr::arrange(order) %>% select(order))) %>% unlist()
 
   # Convert to factor for plotting - with names and levels
-  player_df$order <- factor(player_df$order, levels = levels, labels = labels)
+  df$order <- factor(df$order, levels = levels, labels = labels)
 
   # Create plot with x and y axis
-  ggplot(player_df, aes(x = as.factor(.data[[season]]), y = .data[[value]], group = 1)) +
+  ggplot(df, aes(x = as.factor(.data[[season]]), y = .data[[value]], group = 1)) +
     # Add points to graph
     geom_point(aes(), size = 2, alpha = 0.3) +
     # Add data points to graph
-    geom_text_repel(data = player_df, aes(label = .data[[value]]), size = 5, nudge_x = 0.1) +
+    geom_text_repel(data = df, aes(label = .data[[value]]), size = 5, nudge_x = 0.1) +
     # Connect points in the same competition over time
     geom_path() +
     # Facet the plot by competition, ordered by divforRpack::comp_order() and named by competition coloum
